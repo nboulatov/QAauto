6298206cb906aa89b5e7e2712e291bd0f40b87ef
nboulatov <nboulatov@gmail.com>
2023-11-15 09:49:56 -0800

    L2 HM

--- README.md
+++
@@ -1,2 +0,0 @@
-# QAauto
-QA Automation Class
---
+++ testing.py
@@ -0,0 +1,14 @@
+number = 1597
+
+digit_1 = number % 10
+reduced_once = number // 10
+digit_2 = reduced_once % 10
+reduced_twice = reduced_once // 10
+digit_3 = reduced_twice % 10
+reduced_thrice = reduced_twice // 10
+digit_4 = reduced_thrice % 10
+print(number)
+print(digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
